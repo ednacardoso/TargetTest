f8784f81caed120e0c3222a07e980d929665c238
ednacardoso <71801131+ednacardoso@users.noreply.github.com>
2025-01-16 12:07:31 -0300

    Create Layout.tsx

---
+++ src/Layout.tsx
@@ -0,0 +1,14 @@
+import { Link, Outlet } from 'react-router-dom'
+
+function Layout() {
+  return (
+    <div>
+      <nav>
+        <Link to="/">Home</Link>
+      </nav>
+      <Outlet />
+    </div>
+  )
+}
+
+export default Layout;
